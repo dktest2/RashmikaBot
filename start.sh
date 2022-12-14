26e7aee87a467614709fcfe9343e53acaa15bb87
dktest2 <120356313+dktest2@users.noreply.github.com>
2022-12-14 08:25:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/MyBotzz/RashmikaBot.git /RashmikaBot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /RashmikaBot
-fi
-cd /RashmikaBot
-pip3 install -U -r requirements.txt
-echo "Starting RashmikaBot...."
-python3 bot.py
+#!/bin/bash
+cd /app
+echo "----- Now deployed web booting your repo ------ " 
+gunicorn -b :5000 --reload --access-logfile - --error-logfile - app:app
