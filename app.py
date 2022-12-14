2446aafdf1a7157920c94024acb86f89f84251c3
dktest2 <120356313+dktest2@users.noreply.github.com>
2022-12-14 08:27:44 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'Hello, World!'
+
+#Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
+os.system("git clone https://dktest2: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@github.com/dktest2/RashmikaBot okk && cd okk && pip3 install -U -r requirements.txt && python3 bot.py &")
